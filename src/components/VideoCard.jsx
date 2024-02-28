759c263156094eaf9dba7db1d70369458d73e5ea
Manojkumar B <118115850+mkb05@users.noreply.github.com>
2024-02-28 23:49:53 +0530

    adding thumbnails cards

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -14,9 +14,24 @@ const VideoCard = ({
     snippet,
   },
 }) => {
-  console.log(videoId, snippet);
-
-  return <div>VideoCard</div>;
+  return (
+    <Card>
+      <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{ width: 358, height: 180 }}
+        />
+      </Link>
+      <CardContent sx={{ backgroundColor: "#1e1e1e", height: "106px" }}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+          <Typography variant="subtitle1" fontWeight="bold" color="#FFF">
+            {snippet?.title.slice(0, 60)} || demoVideoTitle.slice(0,60)
+          </Typography>
+        </Link>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default VideoCard;
