a2668d267f93c407abfacbed364d173f85c57ac1
Manojkumar B <118115850+mkb05@users.noreply.github.com>
2024-03-07 23:10:43 +0530

    search Feed

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
+
+import Videos from "./Videos";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
+
+  return (
+    <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+      <Typography
+        variant="h4"
+        fontWeight="bold"
+        mb={2}
+        sx={{
+          color: "white",
+        }}
+      >
+        {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
-export default SearchFeed;
+export default Feed;
